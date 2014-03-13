2aaf41faabc8ab09f5e0b68c9b836212aa2ea451
Dan Fan <dan.fan1988@gmail.com>
2014-03-13 15:55:56 -0700

    added reply and touched put some css

--- javascript/main.js
+++ javascript/main.js
@@ -15,17 +15,52 @@
 				var response = data.topics[i].responses[j]
 				if (response.parentid == 0) {
 					$('ul[data-title-id=' + i + ']')
-					.append('<li data-post-id=' + response.id + '>' + response.posttext)
+					.append('<li data-post-id=' + response.id + '>' + response.posttext + 
+						'<div class="author"><strong>Posted by: </strong>' + response.author + 
+						'</div><div class="time">' + this.parseAge(response.age) + 
+						'</div><a href="#">Reply</a>')
 				} else if (response.parentid > 0) {
 					$('li[data-post-id=' + response.parentid + ']')
-					.append('<ul><li data-post-id=' + response.id + '>' + response.posttext)
+					.append('<ul><li data-post-id=' + response.id + '>' + response.posttext + 
+						'<div class="author"><strong>Posted by: </strong>' + response.author +
+						'</div><div class="time">' + this.parseAge(response.age) +
+						'</div><a href="#">Reply</a>')
 				}
 			}
 		}
 		$('.title').on("click", function(event){
 			$(this).next('li').toggle()
 		});
+
+		$('a').on("click", function(event){
+			event.preventDefault();
+			$(this).replaceWith("<form><input type='text' placeholder='Username'><br><textarea placeholder='Post'></textarea>" +
+				"<br><input type='submit' value='Post'></form>")
+			$('form').on('submit', function(event){
+				event.preventDefault();
+				if (!event.target[0].value || !event.target[1].value) {
+					alert("You must provide a username and/or post")
+				} else {
+					$(this).html("<ul><li>" + event.target[1].value +
+															 '<div class="author"><strong>Posted by: </strong>' + event.target[0].value + 
+															'</div><div class="time">' + that.parseAge(0))
+				}
+			})
+		});
+
 	};
+
+	View.prototype.parseAge = function(string){
+		var age = parseInt(string)
+
+		var days = Math.floor(age/86400)
+		var hours = Math.floor((age - days * 86400) / 3600)
+		var minutes = Math.floor((age - days * 86400 - hours * 3600) / 60)
+		var seconds = age - days * 86400 - hours * 3600 - minutes * 60
+
+		return "Posted " + days + " days, " + hours + " hours, " + minutes + " minutes, and " 
+							 + seconds + " seconds ago."
+	}
 
 	View.prototype.initialize = function(){
 		var that = this;
